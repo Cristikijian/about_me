451ca462438e68019eb06cce41ce02b0486aeb88
Cristikijian <cristikijian@gmail.com>
2024-02-22 16:59:47 +0300

    webpack_added

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+$.when( $.ready ).then(function() {
+  const tabs = $('.main-page__tablink');
+  tabs.on('click', function(eventObject) {
+    eventObject.preventDefault();
+    tabs.removeClass("active");
+    $(this).addClass("active");
+    $(".tab").hide();
+    $(`[data-id="${this.id}"]`).fadeIn()
+  });
+});
