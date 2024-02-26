06abc662383d9d562380cf2b7ed7a85a20d6405c
Cristikijian <cristikijian@gmail.com>
2024-02-26 12:19:04 +0300

    styles refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 $.when( $.ready ).then(function() {
-  const tabs = $('.main-page__tablink');
-  tabs.on('click', function(eventObject) {
+  const tabs = $(".main-page__tablink");
+  tabs.on("click", function(eventObject) {
     eventObject.preventDefault();
     tabs.removeClass("active");
     $(this).addClass("active");
-    $(".tab").hide();
-    $(`[data-id="${this.id}"]`).show()
+    $(".tab").fadeOut();
+    $(`[data-id="${this.id}"]`).fadeIn()
+  });
+
+  $(".show-button").on("click", function(eventObject) {
+    eventObject.preventDefault();
+    const button = $(eventObject.target);
+    button.siblings('.hiding-wrapper').fadeIn();
+    button.siblings('.hide-button').show();
+    button.hide();
+  });
+
+  $(".hide-button").on("click", function(eventObject) {
+    eventObject.preventDefault();
+    const button = $(eventObject.target);
+    button.siblings('.hiding-wrapper').fadeOut();
+    button.siblings('.show-button').show();
+    button.hide();
   });
 });
